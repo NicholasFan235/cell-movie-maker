9c973257d0153faee0136894871341e4429b9b40
Nicholas Fan <nicholas.lj.fan@gmail.com>
2022-11-29 11:30:14 +0000

    refactor

--- cell_movie_maker/timepoint_plotter.py
+++ cell_movie_maker/timepoint_plotter.py
@@ -33,3 +33,17 @@ class TimepointPlotter:
         self.plot_stroma(ax, simulation_timepoint)
         self.plot_tumour(ax, simulation_timepoint)
         self.plot_cytotoxic(ax, simulation_timepoint)
+    
+    def cytotoxic_histogram(self, ax, simulation_timepoint):
+        data = simulation_timepoint.cytotoxic_data.potency.to_numpy()
+        data[data < 0] = 0
+        ax.hist(data, bins=10, range=(0,1), log=True)
+        ax.set_xscale('log')
+        ax.set_xlabel('CD8+ Potency')
+    
+    def tumour_histogram(self, ax, simulation_timepoint):
+        data = simulation_timepoint.tumour_data.damage.to_numpy()
+        data[data > 1] = 1
+        ax.hist(data, bins=10, range=(0,1), log=True)
+        ax.set_xscale('log')
+        ax.set_xlabel('Tumour Accumulated Damage')
