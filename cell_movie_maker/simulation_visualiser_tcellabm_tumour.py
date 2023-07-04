863581066f364aaddd3d1dbbc64f0011e89841ee
Nicholas Fan <fann@maths.ox.ac.uk>
2023-07-04 17:25:58 +0100

    update

---
+++ cell_movie_maker/simulation_visualiser_tcellabm_tumour.py
@@ -0,0 +1,56 @@
+
+from .simulation import Simulation
+from .simulation_visualiser import AbstractSimulationVisualiser
+from .timepoint_plotter import TimepointPlotter, HistogramPlotter, TumourTimepointPlotter
+from .timepoint_plotter_v2 import TimepointPlotterV2, TumourTimepointPlotterV2
+import matplotlib.pylab as plt
+import os
+import shutil
+import numpy as np
+
+
+class TCellABMTumourVisualiser(AbstractSimulationVisualiser):
+    def __init__(self, simulation:Simulation, visualisation_name='standard', **kwargs):
+        super().__init__(simulation, visualisation_name=visualisation_name, **kwargs)
+        self.figsize=(12,8)
+
+    def visualise_frame(self, info):
+        frame_num, timepoint = info
+        simulation_timepoint = self.sim.read_timepoint(timepoint)
+
+        fig, axs = plt.subplot_mosaic("AAB;AAC", figsize=self.figsize)
+        #ax.margins(0.01)
+        self.tp.plot(fig, axs['A'], simulation_timepoint, frame_num, timepoint)
+
+        axs['B'].set_title('Oxygen')
+        axs['B'].set_yticks([])
+        axs['B'].set_xticks([])
+        ox = axs['B'].imshow(simulation_timepoint.read_pde('oxygen'), cmap='cividis', vmin=0, vmax=1, origin='lower')
+        fig.colorbar(ox, ax=axs['B'])
+
+        axs['C'].set_title('Pressure')
+        axs['C'].set_yticks([])
+        axs['C'].set_xticks([])
+        pressure_data = simulation_timepoint.read_pde('pressure')
+        pressure = axs['C'].imshow(pressure_data, cmap='magma', vmin=0, vmax=pressure_data.max(), origin='lower')
+        fig.colorbar(pressure, ax=axs['C'])
+
+
+        if self.postprocess is not None:
+            self.postprocess(fig, axs)
+
+        fig.tight_layout()
+        fig.savefig(os.path.join(self.output_folder, 'frame_{}.png'.format(frame_num)))
+        plt.close(fig)
+        return
+
+    def visualise(self, auto_execute=True, maxproc=64, *args, **kwargs):
+        super().visualise(*args, **kwargs)
+
+        #self.tp = TimepointPlotter(marker='o', edgecolors='black', linewidths=0.2, s=20)
+        #self.tp.cmap=True
+        self.tp = TimepointPlotterV2()
+
+        if auto_execute:
+            self.sim.for_timepoint(self.visualise_frame, start=self.start, stop=self.stop, step=self.step, maxproc=maxproc)
+
