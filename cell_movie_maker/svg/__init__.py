9c51494ccc2afd5c71d413680ddb37fb57a32240
Nicholas Fan <nicholas.lj.fan@gmail.com>
2023-07-23 04:00:29 +0100

    svg stitchers

--- cell_movie_maker/svg/__init__.py
+++ cell_movie_maker/svg/__init__.py
@@ -1,2 +1,3 @@
 from .svg_visualiser import SVGVisualiser
 from .svg_writer import SVGWriter, TumourSVGWriter, HypoxiaSVGWriter, PressureSVGWriter, OxygenSVGWriter, CCL5SVGWriter
+from .svg_stitcher import TCellSVGStitcher, TumourSVGStitcher
