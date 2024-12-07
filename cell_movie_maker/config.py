83a9cbe2636498bc40e6dd8e1c597a5ca14eb3fc
Nicholas Fan <fann@maths.ox.ac.uk>
2024-12-07 07:07:31 +0000

    config update

--- cell_movie_maker/config.py
+++ cell_movie_maker/config.py
@@ -7,6 +7,6 @@ def env_var(key, default=""):
     return os.environ[key] if key in os.environ else default
 
 class Config:
-    simulations_folder:pathlib.Path=pathlib.Path(env_var("CHASTE_TEST_OUTPUT"))
-    output_folder:pathlib.Path=pathlib.Path(env_var('CHASTE_VISUALISATIONS_OUTPUT'))
+    simulations_folder:pathlib.Path=pathlib.Path(env_var("CMM_SIMULATIONS_DIR"))
+    output_folder:pathlib.Path=pathlib.Path(env_var('CMM_OUTPUT_DIR'))
     
