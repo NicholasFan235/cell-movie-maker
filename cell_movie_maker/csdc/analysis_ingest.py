74b423b49bdab749c97982225086305bdf6e3460
Nicholas Fan <fann@maths.ox.ac.uk>
2024-12-27 23:35:22 +0000

    feature to skip existing analysis

--- cell_movie_maker/csdc/analysis_ingest.py
+++ cell_movie_maker/csdc/analysis_ingest.py
@@ -3,10 +3,12 @@ from ..experiment import Experiment
 from ..simulation import Simulation
 from ..simulation_timepoint import SimulationTimepoint
 import typing
+import pandas as pd
 
 class AnalysisIngest:
-    def __init__(self, db:csdc.Connection):
+    def __init__(self, db:csdc.Connection, skip_existing=False):
         self.db:csdc.Connection = db
+        self.skip_existing = skip_existing
 
     def ingest_experiment(self, experiment:Experiment, *args, **kwargs):
         raise NotImplementedError
@@ -17,4 +19,10 @@ class AnalysisIngest:
     def ingest_timepoint(self, timepoint:SimulationTimepoint, *args, **kwargs):
         raise NotImplementedError
     
-    
+    def get_skip_sims(self, experiment:Experiment, analysis_name:str):
+        if self.skip_existing: return set()
+        experiment = str(experiment)
+        skip_ids = pd.read_sql("SELECT iteration FROM analysis INNER JOIN simulations ON analysis.simulation_id = simulations.id WHERE analysis_name = :analysis_name AND experiment = :experiment",
+                    self.db.get_connection(), params=dict(analysis_name = analysis_name, experiment=experiment))
+        self.db.close_connection()
+        return set(skip_ids.iteration)
