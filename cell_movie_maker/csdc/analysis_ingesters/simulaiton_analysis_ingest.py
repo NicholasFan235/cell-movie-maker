20fea61e4ee862c60c3bd09074f9f5ab6278b3e5
Nicholas Fan <fann@maths.ox.ac.uk>
2024-12-27 23:35:22 +0000

    feature to skip existing analysis

--- cell_movie_maker/csdc/analysis_ingesters/simulaiton_analysis_ingest.py
+++ cell_movie_maker/csdc/analysis_ingesters/simulaiton_analysis_ingest.py
@@ -20,7 +20,7 @@ def chunk(l, n):
 def process_sim(info:tuple):
     sim:Simulation = info[0]
     analyser:typing.Type[SimulationAnalyser] = info[1]
-    return dict(experiment=sim.name, iteration=sim.iteration, analysis_name=str(analyser), analysis_value=analyser.analyse(sim).to_json())
+    return dict(experiment=sim.name, iteration=sim.iteration, analysis_name=str(analyser), analysis_value=analyser.analyse(sim).to_json(), timestep=None)
 
 class SimulationAnalysisIngest(AnalysisIngest):
     def __init__(self, *args, **kwargs):
@@ -28,14 +28,16 @@ class SimulationAnalysisIngest(AnalysisIngest):
         self.nproc = 50
         self.batch_size = 500
 
-
     def ingest_experiment(self, experiment:Experiment, analyser:typing.Type[SimulationAnalyser]):
-        for i, sims_batch in enumerate(chunk(experiment.simulations, self.batch_size)):
-            logging.info(f"Batch {i}...")
+        skip_sim_ids = self.get_skip_sims(str(analyser))
+
+        for i, sims_batch in enumerate(chunk(experiment.sim_ids, self.batch_size)):
+            to_process = [experiment.read_simulation(sim_id) for sim_id in sims_batch if sim_id not in skip_sim_ids]
+            logging.info(f"Batch {i}, Performing {len(to_process)} new analysis...")
             with multiprocessing.Pool(self.nproc, maxtasksperchild=1) as p:
                 analysis = list(tqdm.tqdm(
-                    p.imap(process_sim, zip(sims_batch, itertools.repeat(analyser))),
-                    total=len(sims_batch)))
+                    p.imap(process_sim, zip(to_process, itertools.repeat(analyser))),
+                    total=len(to_process)))
             analysis = [r for r in analysis if r is not None]
 
             self.db.add_bulk_analysis(analysis, commit=True, close_connection=True)
