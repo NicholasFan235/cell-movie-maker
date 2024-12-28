d21555048701a4659bbe5f1cbb6cb0f1a8bbd037
Nicholas Fan <nicholas.lj.fan@gmail.com>
2024-12-28 00:54:39 +0000

    analysis framework

---
+++ cell_movie_maker/analysers/timepoint_analysers/tcell_tcell_pcf_analyser.py
@@ -0,0 +1,36 @@
+import chaste_simulation_database_connector as csdc
+from ..simulation_analyser import SimulationAnalyser
+from ...experiment import Experiment
+from ...simulation import Simulation
+from ...simulation_timepoint import SimulationTimepoint
+from ..timepoint_analyser import TimepointAnalyser
+from ..helpers import alpha_shape
+import typing
+import enum
+import multiprocessing
+import tqdm
+import numpy as np
+
+import pandas as pd
+
+
+class TCellTCellPCFAnalyser(TimepointAnalyser):
+    def __init__(self, r_max=50, dr=1, step=1):
+        self.r_max = r_max
+        self.dr = dr
+        self.step = step
+        
+    def analyse(self, timepoint:SimulationTimepoint)->pd.DataFrame|pd.Series|float|int:
+        import muspan as ms
+        domain = timepoint.to_muspan()
+        p1 = ms.query.interpret_query(ms.query.query(domain, "cell_type", "is", "T Cell"))
+        p2 = ms.query.interpret_query(ms.query.query(domain, "cell_type", "is", "T Cell"))
+        if len(p1) == 0 or len(p2) == 0: raise RuntimeError("Cannot calculate PCF with 0 cells")
+        r,g = ms.spatial_statistics.cross_pair_correlation_function(
+            domain,
+            p1, p2,
+            max_R = self.r_max, annulus_width=self.dr, annulus_step=self.step)
+        return pd.DataFrame.from_dict({'r':r, 'g':g}, orient='columns')
+
+    def __str__(self):
+        return f'TCell-TCell PCF r={self.r_max} dr={self.dr} step={self.step}'
